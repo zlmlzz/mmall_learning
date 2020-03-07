fe477efdc5d300f544ecbdfcd116af75ea858cb0
Z <2327567698@qq.com>
2020-03-07 14:19:26 +0800

    一阶段

---
+++ src/main/java/com/mmall/service/IUserService.java
@@ -0,0 +1,27 @@
+package com.mmall.service;
+
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.User;
+
+public interface IUserService {
+
+    ServerResponse<User> loginin(String username, String password);
+
+    ServerResponse<String> register(User user);
+
+    ServerResponse<String> checkValid(String str, String type);
+
+    ServerResponse<String> selectQuestion(String username);
+
+    ServerResponse<String> checkAnswer(String username, String question, String answer);
+
+    ServerResponse<String> forgetResetPassword(String username, String passwordNew, String forgetToken);
+
+    ServerResponse<String> resetPassword(String passwordOld, String passwordNew, User user);
+
+    ServerResponse<User> updateInfomation(User user);
+
+    ServerResponse<User> getInfomation(Integer userId);
+
+    ServerResponse checkAdminRole(User user);
+}
